fc93fccb03d5849ff21e5ca096f1e175d43dee85
Gabriel-Almeida1 <gabriel.avila@poli.ufrj.br>
2021-02-11 20:48:12 -0300

    deletar carrinho

--- app/carrinhos/controllers.py
+++ app/carrinhos/controllers.py
@@ -10,8 +10,8 @@ from ..itens_carrinho.model import ItensCarrinho
 class UsuarioCarrinho(MethodView): # /carrinho/<int:id>
     def get(self, id):
         user = Usuarios.query.get_or_404(id)
-        dados = request.json
-        if not user.carrinho:
+
+        if not user.carrinho or not user.carrinho.itens:
             return{"Mensagem":"Carrinho Vazio"}, 200
 
         return jsonify([item.json() for item in user.carrinho.itens]),200
@@ -31,7 +31,6 @@ class UsuarioCarrinho(MethodView): # /carrinho/<int:id>
             return{"Erro": "Produto não cadastrado"}, 400
 
         if user.carrinho:
-            print("if")
             carrinho = user.carrinho 
             achou = False
 
@@ -43,31 +42,47 @@ class UsuarioCarrinho(MethodView): # /carrinho/<int:id>
 
             if achou == False:
                 item = ItensCarrinho(nome_produto=nome_produto,preco=produto.preco, unidades=1, carrinho=carrinho)
-                db.session.add(item)
-
-            db.session.commit()
+                db.session.add(item)    
 
         else:
-            print("else")
             carrinho = Carrinhos(usuario=user)
             item = ItensCarrinho(nome_produto=nome_produto,preco=produto.preco, unidades=1, carrinho=carrinho)
             db.session.add(carrinho)
             db.session.add(item)
-            db.session.commit()
+        
+        db.session.commit()
 
         return carrinho.json(), 200
 
+    def delete(self, id): # recebe "produto"
+        # Vai deletar 1 unidade por vez
+        user = Usuarios.query.get_or_404(id)
+        dados = request.json
 
-'''
-    if request.method == 'DELETE':
-         Vai esvaziar o carrinho.
-        if not user.carrinho:
-            return{"Erro":"Carrinho já está Vazio"}, 400
+        nome_produto = dados.get('produto')
+
+        if nome_produto == '' or nome_produto == None or not isinstance(nome_produto, str):
+            return{"Erro":"Nome do produto é obrigatório e deve ser String"}, 400
         
-        for produto in user.carrinho.produtos:
-            print(produto)
-            db.session.delete(produto)
-            db.session.commit()
+        if not user.carrinho:
+            return{"Erro":"Carrinho está vazio"}, 400
+        
+        achou = False
+        for count in range(0,len(user.carrinho.itens),1):
+            if user.carrinho.itens[count].nome_produto == nome_produto:
+                item = user.carrinho.itens[count]
+                achou = True
+                break
+        
+        if achou == False:
+            return{"Erro":"Produto não encontrado no carrinho"}, 400
+        
+        item.unidades -= 1
 
-        return user.json(), 200
-'''
+        if item.unidades == 0:
+            db.session.delete(item)
+        
+        db.session.commit()
+
+        return {"Mensagem":"Item excluído com sucesso"}, 200
+        
--- app/carrinhos/routes.py
+++ app/carrinhos/routes.py
@@ -8,5 +8,5 @@ carrinhos_api = Blueprint('carrinhos_api', __name__)
 carrinhos_api.add_url_rule(
 '/carrinho/<int:id>',
 view_func=UsuarioCarrinho.as_view('carrinho_usuario'),
-methods=['POST','GET']
+methods=['POST','GET','DELETE']
 )
